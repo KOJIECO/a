73fe42c1cd4997d7cbc453783152fdf8b3c81723
KOJIECO <eduard.chiverdin@mail.ru>
2024-03-07 21:23:38 +0600

    AndreiHelperMega9000

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: java -jar target/a-1.0.jar
 
--- src/main/java/com/mycompany/a/B.java
+++ src/main/java/com/mycompany/a/B.java
@@ -6,15 +6,17 @@ package com.mycompany.a;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author eduar
  */
+@WebServlet(name = "B", urlPatterns = {"/B"})
 public class B extends HttpServlet {
 
     /**
